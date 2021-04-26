1f573637253be31d7ff41904e7e57c72380703ec
Sujan Maharjan <thesujan540@gmail.com>
2021-04-26 18:21:37 -0500

    Update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8081'
+  apiUrl: 'http://172.17.0.1:8081'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081'
+  apiUrl: 'http://172.17.0.1:8081'
 };
 
 /*
